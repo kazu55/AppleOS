967134971ee2b2df18f8a9b454f389e951ed4962
たまさん <58621773+kazu55@users.noreply.github.com>
2021-04-03 17:20:57 +0900

    Update Login.lua

--- Login.lua
+++ Login.lua
@@ -1,9 +1,16 @@
-Key = ("VvjhYxsiTyF4")
+Key = "VvjhYxsiTyF4"
 
+if fs.exists("key.txt") == true then
+  shell.run("Desktop")
+end
 print("Please write the key")
 a = read()
 
 if a == Key then
+  local h = fs.open("key.txt", "w")
+  h.write(Key)
+  h.close()
   shell.run("Desktop")
 else
-  shell.run("Login")
+  shell.run("test")
+end
